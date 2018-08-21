3bfb0775bd15adc54be50d3fd7bc741c3d54fbdd
Brian <brian.fitzpatrick.cs@gmail.com>
2018-08-20 23:48:17 -0400

    adding color per pixel

--- tracer.go
+++ tracer.go
@@ -16,15 +16,26 @@ type point struct {
 	x, y, z float32
 }
 
-func convertToFloat32Slice(p []point) []float32 {
-	result := make([]float32, len(p)*3)
+type pixel struct {
+	position point
+	rgb      point
+}
+
+func convertToFloat32Slice(p []pixel) []float32 {
+	result := make([]float32, len(p)*6)
 	ctr := 0
 	for i := 0; i < len(p); i++ {
-		result[ctr] = p[i].x
+		result[ctr] = p[i].position.x
 		ctr++
-		result[ctr] = p[i].y
+		result[ctr] = p[i].position.y
 		ctr++
-		result[ctr] = p[i].z
+		result[ctr] = p[i].position.z
+		ctr++
+		result[ctr] = p[i].rgb.x
+		ctr++
+		result[ctr] = p[i].rgb.y
+		ctr++
+		result[ctr] = p[i].rgb.z
 		ctr++
 	}
 	return result
@@ -33,7 +44,7 @@ func convertToFloat32Slice(p []point) []float32 {
 func main() {
 	var VBO, VAO uint32
 	pixelCount := windowHeight * windowWidth
-	vertices := make([]point, pixelCount)
+	vertices := make([]pixel, pixelCount)
 	xIncrement := float32(2.0) / float32(windowWidth)
 	yIncrement := float32(2.0) / float32(windowHeight)
 	X := float32(-1.0)
@@ -44,15 +55,20 @@ func main() {
 		for j := 0; j < windowHeight; j++ {
 			Y = float32(Y) - yIncrement
 			index := (i * windowHeight) + j
-			vertices[index] = point{X, Y, 0.0}
+			color := point{1.0, 0.0, 0.0}
+
+			if j%10 == 0 {
+				color = point{0.0, 1.0, 0.0}
+			}
+			if i%10 == 0 {
+				color = point{0.0, 0.0, 1.0}
+			}
+			vertices[index] = pixel{position: point{X, Y, 0.0}, rgb: color}
+
+			//compute ray position for this pixel
+			//determine color for that pixel and set in object.
 		}
 		Y = float32(1.0)
-		// newPoint := rand.Float32()
-		// flipSign := rand.Intn(2)
-		// if flipSign == 1 {
-		// 	newPoint *= -1
-		// }
-		// vertices[i] = newPoint
 	}
 	vsize := int32(len(vertices))
 	flatVertex := convertToFloat32Slice(vertices)
@@ -84,15 +100,17 @@ func main() {
 
 	gl.GenBuffers(1, &VBO)                                                                //generate a buffer object
 	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)                                                   //bind the buffer object to a certain buffer
-	gl.BufferData(gl.ARRAY_BUFFER, len(flatVertex)*4, gl.Ptr(flatVertex), gl.STATIC_DRAW) //load up a buffer with vertex data
+	gl.BufferData(gl.ARRAY_BUFFER, len(flatVertex)*4, gl.Ptr(flatVertex), gl.STATIC_DRAW) //load up a buffer with vertex data, need to specify size in bytes (float32 is 4 bytes so multiply by 4)
 
 	//shaders
 	shaderProgram, err := newProgram(vertexShader, fragShader)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, gl.PtrOffset(0))
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(0)
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(3*4))
+	gl.EnableVertexAttribArray(1)
 
 	for !window.ShouldClose() {
 		input(window)
@@ -100,10 +118,10 @@ func main() {
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 
 		gl.UseProgram(shaderProgram)
-		vertexColorLocation := gl.GetUniformLocation(shaderProgram, gl.Str("ourColor\x00"))
+		//vertexColorLocation := gl.GetUniformLocation(shaderProgram, gl.Str("ourColor\x00"))
 		gl.BindVertexArray(VAO)
 		gl.DrawArrays(gl.POINTS, 0, vsize)
-		gl.Uniform4f(vertexColorLocation, 0.0, 0.0, 1.0, 1.0)
+		//gl.Uniform4f(vertexColorLocation, 0.0, 0.0, 1.0, 1.0)
 
 		// Maintenance
 		window.SwapBuffers()
@@ -178,17 +196,20 @@ func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error)
 var vertexShader = `
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+out vec4 ourColor;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	ourColor = vec4(aColor.x, aColor.y, aColor.z, 1.0);
 }
 ` + "\x00"
 
 var fragShader = `
 #version 330 core
 out vec4 FragColor;
-uniform vec4 ourColor; // we set this variable in the OpenGL code.
+in vec4 ourColor; // we set this variable in the OpenGL code.
 
 void main()
 {
